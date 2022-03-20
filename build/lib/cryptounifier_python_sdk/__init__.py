3ea64f4449fd4ed5e044b6d651d5e8c57fcf5b10
BEN AMARA <ben.amara.hamza2011@gmail.com>
2022-03-20 16:23:56 +0100

    remove except class manage

--- build/lib/cryptounifier_python_sdk/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# Package version
-
-from .MerchantAPI import MerchantAPI
-from .WalletAPI import WalletAPI
-from .WalletTokenAPI import WalletTokenAPI
--- cryptounifier_python_sdk/BaseAPI.py
+++ cryptounifier_python_sdk/BaseAPI.py
@@ -56,42 +56,10 @@ class BaseAPI(object):
         url = self._url + '/' + args[0]   
         headers = dict(self.http.headers)
         response = None             
-        try:
-            self._prepare_kwargs(method, args, kwargs)                        
-            response = self.http.request(method, url=url, **kwargs)  
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            logger.debug('Error getting response from ' + url, exc_info=True)
-            status_code = response.status_code if response is not None else 0
-            error_msg = response.content.decode('utf-8') if response is not None else str(e)
-            return ApiResult(url, {}, {'error': error_msg}, headers, 'error', status_code=status_code)
-        status_code = response.status_code
-        try:                      
-            return response.json()
-        except ValueError as e:
-            logger.debug('Error parsing JSON response from '+url+'. Response: ' + str(response.content), exc_info=True)
-            return ApiResult(
-                url, {}, {'error': str(e), 'response': response.content.decode('utf-8')}, headers, 'error',
-                status_code=status_code
-            )        
+        self._prepare_kwargs(method, args, kwargs) 
+                     
+        response = self.http.request(method, url=url, **kwargs)  
+        return response.json()
 
 
-@attr.s
-class ApiResult(object):
-    """
-    Response returned form ML API
-    """
-    url = attr.ib(default='')
-    request = attr.ib(default='')
-    response = attr.ib(default=attr.Factory(dict))
-    headers = attr.ib(default=attr.Factory(dict))
-    type = attr.ib(default='ok')
-    status_code = attr.ib(default=200)
-
-    @property
-    def is_error(self):        
-        return self.status_code != 200
-
-    @property
-    def error_message(self):
-        return self.response.get('error')
+       
